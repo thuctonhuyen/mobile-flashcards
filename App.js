5296c2ef48efc6f73001a64951778d2b46617e83
THUC HUYEN <thuctonhuyen@gmail.com>
2017-10-14 13:03:41 -0700

    first-commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+export default class App extends React.Component {
+  render() {
+    return (
+        <Container>
+            <Header>
+                <Left>
+                    <Button transparent>
+                        <Icon name='menu' />
+                    </Button>
+                </Left>
+                <Body>
+                <Title>Header</Title>
+                </Body>
+                <Right />
+            </Header>
+            <Content>
+                <Text>
+                    This is Content Section
+                </Text>
+            </Content>
+            <Footer>
+                <FooterTab>
+                    <Button full>
+                        <Text>Footer</Text>
+                    </Button>
+                </FooterTab>
+            </Footer>
+        </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
