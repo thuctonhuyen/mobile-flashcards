323db046c2d749895857c86e1d976a88507f73e4
THUC HUYEN <thuctonhuyen@gmail.com>
2017-10-16 16:56:18 -0700

    feat: Add stack navigation between list of decks and individual deck

--- App.js
+++ App.js
@@ -1,40 +1,49 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import {StyleSheet} from 'react-native';
+import {Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text} from 'native-base';
+import reducer from './reducers'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import ListOfDecks from './components/ListOfDecks'
+import IndividualDeck from './components/IndividualDeck'
+import {StackNavigator, TabNavigator } from 'react-navigation'
+
+//tab navigator => later on
+const Tabs = TabNavigator({
+    Home: {
+        screen: ListOfDecks,
+    },
+
+    Deck:{
+        screen: IndividualDeck
+    }
+})
+
+//stack navigator
+const MainNavigator = StackNavigator({
+    Home: {
+        screen: ListOfDecks,
+    },
+
+    Deck:{
+        screen: IndividualDeck
+    }
+
+});
+
+
 export default class App extends React.Component {
-  render() {
-    return (
-        <Container>
-            <Header>
-                <Left>
-                    <Button transparent>
-                        <Icon name='menu' />
-                    </Button>
-                </Left>
-                <Body>
-                <Title>Header</Title>
-                </Body>
-                <Right />
-            </Header>
-            <Content>
-                <Text>
-                    This is Content Section
-                </Text>
-            </Content>
-            <Footer>
-                <FooterTab>
-                    <Button full>
-                        <Text>Footer</Text>
-                    </Button>
-                </FooterTab>
-            </Footer>
-        </Container>
-    );
-  }
+    render() {
+        return (
+            <Provider store={createStore(reducer)}>
+                <Container>
+                    <MainNavigator/>
+                </Container>
+            </Provider>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  },
+    container: {},
 });
