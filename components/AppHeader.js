e126311af66811fb4f3731cef210a7e80af3c49c
THUC HUYEN <thuctonhuyen@gmail.com>
2017-10-18 17:40:30 -0700

    feat: add customize Header component

---
+++ components/AppHeader.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {
+    Container, Title, Header, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text
+    , Card, CardItem, H1, H2, H3
+} from 'native-base';
+
+
+export default class AppHeader extends React.Component {
+
+    render() {
+
+        const {home, header_title, go_back} = this.props;
+
+
+        return (
+            (home) ?
+                (
+                    <Header>
+                        <Body>
+                        <Title>List of Decks</Title>
+                        </Body>
+                    </Header>
+                )
+                :
+                (
+                    <Header>
+                        <Left>
+                            <Button transparent onPress={() => go_back()}>
+                                <Icon name="arrow-back"/>
+                            </Button>
+                        </Left>
+                        <Body>
+                        <Title>{header_title}</Title>
+                        </Body>
+                        <Right/>
+                    </Header>
+
+
+                )
+
+        );
+    }
+}
+
+
